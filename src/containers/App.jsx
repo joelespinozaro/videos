d226e462780f0190d4b9375f254070bcc3719792
Joel Espinoza Rojas <joelespinozaro@gmail.com>
2021-01-14 23:24:30 -0500

    new hook: useInitialState

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -4,31 +4,25 @@ import Search from '../components/Search';
 import Header from '../components/Header';
 import Categories from '../components/Categories';
 import Footer from '../components/Footer/Footer';
+import useInitialState from '../hooks/useInitialState';
 
 const API = 'http://localhost:3000/initialState';
 
 const App = () => {
-  const [videos, setVideos] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    fetch(API)
-      .then((response) => response.json())
-      .then((data) => {
-        setVideos(data);
-        setLoading(false);
-      });
-  }, []);
+  const initialState = useInitialState(API);
 
   return (
     <div className="App">
       <Header />
       <Search />
-      {videos.mylist && videos.mylist.length > 0 && (
-        <Categories title="Mi lista" videos={videos.mylist} />
+      {initialState.mylist && initialState.mylist.length > 0 && (
+        <Categories title="Mi lista" videos={initialState.mylist} />
       )}
-      <Categories title="Tendencias" videos={videos.trends} />
-      <Categories title="Originales" videos={videos.originals} />
+      <Categories title="Tendencias" videos={initialState.trends} />
+      <Categories
+        title="Originales"
+        videos={initialState.originals}
+      />
       <Footer />
     </div>
   );
